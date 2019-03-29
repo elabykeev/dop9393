25d97dcf6f8a9a551c2923071d463020ececdcd2
elabykeev <elamanabykeev8@gmail.com>
2019-03-29 12:53:31 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	final String veriable1 = "привет,";
+	String veriable2 = "мир !";
+	String veriable = veriable1 +  veriable2;
+	System.out.println(veriable);
+	System.out.println(veriable1);
+
+    }
+}
