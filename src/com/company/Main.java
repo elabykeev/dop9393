13ca66e473e64cb02d2080c9a01b70a23afce8f3
elabykeev <elamanabykeev8@gmail.com>
2019-03-29 14:11:41 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	final String veriable1 = "привет,";
-	String veriable2 = "мир !";
-	String veriable = veriable1 +  veriable2;
-	System.out.println(veriable);
+	final String veriable1 = "здравствуйте,";
+	String verible2 = "привет ,";
+	String veriable3 = "как вас завут ?";
+	String veriable4 = "Саша";
+	String veriable5 = verible2 + veriable4;
+
+	System.out.println(veriable1);
+	System.out.println(verible2);
+	System.out.println(veriable3);
+	System.out.println(veriable4);
+	System.out.println(veriable5);
+
+
+
+
 
     }
 }
